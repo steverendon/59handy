fb4c4fa309cb2f6d2a9df75dd2adc27235e72527
Steven Rendon <steven.rendon>
2020-02-28 10:12:19 -0500

    cambios

--- php/availability/handy_services.php
+++ php/availability/handy_services.php
@@ -34,7 +34,7 @@
                 $_SESSION['zip'] = $zipcode;
                 session_destroy();
                 //include("no_service_zone.php");
-                header("location:../../zipcode.html?message=Sorry! there is no Handy Services in this moment for your area, zipcode:".$zipcode);
+                header("location:../../zipcode.html?message=Sorry! there is no Handy Services in this moment for your area, zipcode: $zipcode");
             }
             break;
             
